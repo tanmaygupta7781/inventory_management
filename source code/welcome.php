3d2acc5d991081cd6b015743957f9052492381b4
tanmaygupta7781 <tanmaygupta2610@gmail.com>
2024-09-15 12:10:17 +0530

    2nd

---
+++ source code/welcome.php
@@ -0,0 +1,12 @@
+
+<?php
+
+
+
+// Grab User submitted information
+$email = $_POST["username"];
+$pass = $_POST["password"];
+
+if($email=='abcd'&& $pass=='123')
+echo "Connected successfully";
+?>
